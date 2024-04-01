900b75ff670d23b924459d0070ff00c902d52ab1
刘泽 <liuze@liuzedeMac-mini.local>
2024-04-01 23:30:16 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { IDeadline } from "./utils/polyfill"
+
+const { polyfillRequestIdleCallback } = require("./utils/polyfill")
+
+// polyfill
+polyfillRequestIdleCallback()
+
+// 调度
+const scheduler = (deadline: IDeadline) => {
+    if(!deadline.didTimeout && deadline.timeRemaining() > 0) {
+        console.log(deadline.timeRemaining())
+        // console.log('hello!')
+    }
+    console.log('frame end')
+    window.requestIdleCallback(scheduler)
+}
+window.requestIdleCallback(scheduler)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, '../build'),
+        filename: "[name]-[chunkhash:8]-bundle.js",
+        // publicPath: '/manager',
+        assetModuleFilename: '[name]-[hash:8][ext]',
+        chunkFilename: 'async/[name]-[chunkhash:8].js',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.jsx', '.vue', '.js', '.ts', '.tsx'],
+        mainFiles: ['index'],
+        alias: {
+            utils: path.resolve(__dirname, '../src/utils'),
+            '@': path.resolve(__dirname, '../src/'),
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './template.html'
+        })
+    ]
+}
