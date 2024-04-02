b2f0b23e8da9a921ad739545c18d75a7c46eb215
Gravity2333 <1491310384@qq.com>
2024-04-02 23:14:01 +0800

    fix: 优化demo样式

---
+++ src/components/Counter/index.ts
@@ -0,0 +1,35 @@
+import { createElement } from "../../../lib/React";
+import useState from "../../../lib/hooks/useState";
+
+export default function Counter() {
+    const [value, setValue] = useState(10);
+    const [user, setUser] = useState("TEST");
+
+    const handleAdd = (number) => {
+        setValue((prev) => prev + number);
+    };
+
+    const handleMinus = (number) => {
+        setValue((prev) => prev - number);
+    };
+
+    const handeleInput = (e) => {
+        setUser(e.target.value);
+    };
+
+    return createElement("div", {}, [
+        createElement("div", {}, [
+            createElement("h2", {}, ["计数器:", value]),
+            createElement("button", { onClick: handleAdd.bind(null, 1) }, ["+1"]),
+            createElement("button", { onClick: handleAdd.bind(null, 5) }, ["+5"]),
+            createElement("button", { onClick: handleAdd.bind(null, 8) }, ["+8"]),
+            createElement("button", { onClick: handleMinus.bind(null, 1) }, ["-1"]),
+            createElement("button", { onClick: handleMinus.bind(null, 5) }, ["-5"]),
+            createElement("button", { onClick: handleMinus.bind(null, 8) }, ["-8"]),
+        ]),
+        createElement("div", {}, [
+            createElement("input", { value: user, onInput: handeleInput }, []),
+            createElement("h2", { style: "color:pink" }, ["输入框值:", user]),
+        ]),
+    ]);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,6 @@
 import { createElement } from "../lib/React";
 import { render } from "../lib/ReactDom";
-import useState from "../lib/hooks/useState";
-
-function App() {
-  const [value, setValue] = useState(10);
-  const [user, setUser] = useState("TEST");
-
-  const handleAdd = (number) => {
-    setValue((prev) => prev + number);
-  };
-
-  const handleMinus = (number) => {
-    setValue((prev) => prev - number);
-  };
-
-  const handeleInput = (e) => {
-    setUser(e.target.value);
-  };
-
-  return createElement("div", {}, [
-    createElement("div", {}, [
-      createElement("h2", {}, ["计数器:", value]),
-      createElement("button", { onClick: handleAdd.bind(null, 1) }, ["+1"]),
-      createElement("button", { onClick: handleAdd.bind(null, 5) }, ["+5"]),
-      createElement("button", { onClick: handleAdd.bind(null, 8) }, ["+8"]),
-      createElement("button", { onClick: handleMinus.bind(null, 1) }, ["-1"]),
-      createElement("button", { onClick: handleMinus.bind(null, 5) }, ["-5"]),
-      createElement("button", { onClick: handleMinus.bind(null, 8) }, ["-8"]),
-    ]),
-    createElement("div", {}, [
-      createElement("input", { value: user, onInput: handeleInput }, []),
-      createElement("h2", { style: "color:pink" }, ["输入框值:", user]),
-    ]),
-  ]);
-}
+import App from "./App";
 
 render(createElement(App, {}, []), document.querySelector("#root-master"));
 
